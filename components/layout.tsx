a372421e043e12665923c386cc58cae75f2c840a
kiikoh <ikikomonkey@gmail.com>
2022-06-26 10:05:53 -0400

    update head

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Navbar from './navbar';
 
 interface LayoutProps {
@@ -6,6 +7,12 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({children}: LayoutProps) => (
 	<div>
+		<Head>
+			<title>KiiKoh | Kyle DePace</title>
+			<meta name='description' content='Personal Portfolio of Kyle DePace' />
+			<link rel='icon' href='/favicon.ico' />
+		</Head>
+
 		<Navbar />
 		<div className='container mx-auto'>
 			{children}
